6f338a0d76b99f84881d2dff4d00c72da89e211a
Eugene Birondo <jbirondo@gmail.com>
2020-05-05 17:33:21 -0700

    Getting elements

---
+++ scripts.js
@@ -0,0 +1,5 @@
+const player = document.querySelector('.player')
+const video = player.querySelector('.viewer')
+const progess = player.querySelector('.progress')
+const progessBar = player.querySelector('.progress__filled')
+
